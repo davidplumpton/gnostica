652654bea23ba6c975ad3778642b5180731c2156
David Plumpton <david.plumpton@gmail.com>
2026-05-19 19:49:56 +1200

    Render 3D tarot board scene

--- src/main/externs/three.ext.js
+++ src/main/externs/three.ext.js
@@ -19,3 +19,200 @@ var THREE = {};
  * @const
  */
 THREE.REVISION;
+
+/** @constructor */
+THREE.Scene = function() {};
+
+/**
+ * @param {!Object} object
+ * @return {void}
+ */
+THREE.Scene.prototype.add = function(object) {};
+
+/**
+ * @constructor
+ * @param {number} fov
+ * @param {number} aspect
+ * @param {number} near
+ * @param {number} far
+ */
+THREE.PerspectiveCamera = function(fov, aspect, near, far) {};
+
+/** @type {!Object} */
+THREE.PerspectiveCamera.prototype.position;
+
+/** @type {number} */
+THREE.PerspectiveCamera.prototype.aspect;
+
+/**
+ * @param {number} x
+ * @param {number} y
+ * @param {number} z
+ * @return {void}
+ */
+THREE.PerspectiveCamera.prototype.lookAt = function(x, y, z) {};
+
+/** @return {void} */
+THREE.PerspectiveCamera.prototype.updateProjectionMatrix = function() {};
+
+/**
+ * @constructor
+ * @param {!Object=} parameters
+ */
+THREE.WebGLRenderer = function(parameters) {};
+
+/** @type {!Element} */
+THREE.WebGLRenderer.prototype.domElement;
+
+/**
+ * @param {number} value
+ * @return {void}
+ */
+THREE.WebGLRenderer.prototype.setPixelRatio = function(value) {};
+
+/**
+ * @param {number} width
+ * @param {number} height
+ * @param {boolean=} updateStyle
+ * @return {void}
+ */
+THREE.WebGLRenderer.prototype.setSize = function(width, height, updateStyle) {};
+
+/**
+ * @param {number} color
+ * @param {number=} alpha
+ * @return {void}
+ */
+THREE.WebGLRenderer.prototype.setClearColor = function(color, alpha) {};
+
+/**
+ * @param {!THREE.Scene} scene
+ * @param {!THREE.PerspectiveCamera} camera
+ * @return {void}
+ */
+THREE.WebGLRenderer.prototype.render = function(scene, camera) {};
+
+/** @return {void} */
+THREE.WebGLRenderer.prototype.dispose = function() {};
+
+/**
+ * @constructor
+ * @param {number} width
+ * @param {number} height
+ */
+THREE.PlaneGeometry = function(width, height) {};
+
+/** @return {void} */
+THREE.PlaneGeometry.prototype.dispose = function() {};
+
+/**
+ * @constructor
+ * @param {!Object=} parameters
+ */
+THREE.MeshBasicMaterial = function(parameters) {};
+
+/** @type {?THREE.Texture} */
+THREE.MeshBasicMaterial.prototype.map;
+
+/** @type {!THREE.Color} */
+THREE.MeshBasicMaterial.prototype.color;
+
+/** @type {boolean} */
+THREE.MeshBasicMaterial.prototype.needsUpdate;
+
+/** @return {void} */
+THREE.MeshBasicMaterial.prototype.dispose = function() {};
+
+/**
+ * @constructor
+ * @param {!THREE.PlaneGeometry} geometry
+ * @param {!THREE.MeshBasicMaterial} material
+ */
+THREE.Mesh = function(geometry, material) {};
+
+/** @type {!THREE.Vector3} */
+THREE.Mesh.prototype.position;
+
+/** @type {!THREE.Euler} */
+THREE.Mesh.prototype.rotation;
+
+/** @constructor */
+THREE.TextureLoader = function() {};
+
+/**
+ * @param {string} url
+ * @param {function(!THREE.Texture)=} onLoad
+ * @param {function(*):void=} onProgress
+ * @param {function(*):void=} onError
+ * @return {!THREE.Texture}
+ */
+THREE.TextureLoader.prototype.load = function(url, onLoad, onProgress, onError) {};
+
+/** @constructor */
+THREE.Texture = function() {};
+
+/** @type {!HTMLImageElement} */
+THREE.Texture.prototype.image;
+
+/** @type {!THREE.Vector2} */
+THREE.Texture.prototype.repeat;
+
+/** @type {!THREE.Vector2} */
+THREE.Texture.prototype.offset;
+
+/** @type {*} */
+THREE.Texture.prototype.minFilter;
+
+/** @type {*} */
+THREE.Texture.prototype.magFilter;
+
+/** @type {boolean} */
+THREE.Texture.prototype.needsUpdate;
+
+/** @return {void} */
+THREE.Texture.prototype.dispose = function() {};
+
+/** @constructor */
+THREE.Vector2 = function() {};
+
+/**
+ * @param {number} x
+ * @param {number} y
+ * @return {void}
+ */
+THREE.Vector2.prototype.set = function(x, y) {};
+
+/** @constructor */
+THREE.Vector3 = function() {};
+
+/** @type {number} */
+THREE.Vector3.prototype.z;
+
+/**
+ * @param {number} x
+ * @param {number} y
+ * @param {number} z
+ * @return {void}
+ */
+THREE.Vector3.prototype.set = function(x, y, z) {};
+
+/** @constructor */
+THREE.Euler = function() {};
+
+/** @type {number} */
+THREE.Euler.prototype.z;
+
+/** @constructor */
+THREE.Color = function() {};
+
+/**
+ * @param {number} color
+ * @return {void}
+ */
+THREE.Color.prototype.set = function(color) {};
+
+/** @const {*} */
+THREE.DoubleSide;
+
+/** @const {*} */
+THREE.LinearFilter;
--- src/main/gnostica/app.cljs
+++ src/main/gnostica/app.cljs
@@ -2,6 +2,7 @@
   (:require [gnostica.board :as board]
             [gnostica.cards :as cards]
             [re-frame.core :as rf]
+            [reagent.core :as r]
             [reagent.dom :as rdom]))
 
 (rf/reg-event-db
@@ -15,6 +16,16 @@
  (fn [db [_ index]]
    (assoc db :selected-board-index index)))
 
+(rf/reg-event-db
+ ::clear-three-texture-errors
+ (fn [db _]
+   (assoc db :three-texture-errors [])))
+
+(rf/reg-event-db
+ ::three-texture-error
+ (fn [db [_ image]]
+   (update db :three-texture-errors (fnil conj []) image)))
+
 (rf/reg-sub
  ::board
  (fn [db _]
@@ -24,6 +35,11 @@
  ::selected-board-index
  (fn [db _]
    (:selected-board-index db)))
+
+(rf/reg-sub
+ ::three-texture-errors
+ (fn [db _]
+   (:three-texture-errors db)))
 
 (rf/reg-sub
  ::selected-board-cell
@@ -45,6 +61,172 @@
 (defn three-revision []
   (some-> (three-runtime) (.-REVISION)))
 
+(def card-short 1)
+(def card-long 1.5)
+(def card-gap 0.14)
+(def card-step
+  (+ (/ (+ card-short card-long) 2) card-gap))
+
+(def board-plane-size
+  (+ (* 2 card-step) card-long (* 2 card-gap)))
+
+(defn- card-position [{:keys [row col]}]
+  [(* (- col 1) card-step)
+   (* (- 1 row) card-step)])
+
+(defn- texture-cover! [texture]
+  (let [image (.-image texture)
+        image-width (.-width image)
+        image-height (.-height image)
+        card-aspect (/ card-short card-long)]
+    (when (and (pos? image-width) (pos? image-height))
+      (let [image-aspect (/ image-width image-height)
+            [repeat-x repeat-y] (if (> image-aspect card-aspect)
+                                  [(/ card-aspect image-aspect) 1]
+                                  [1 (/ image-aspect card-aspect)])]
+        (.set (.-repeat texture) repeat-x repeat-y)
+        (.set (.-offset texture)
+              (/ (- 1 repeat-x) 2)
+              (/ (- 1 repeat-y) 2)))))
+  texture)
+
+(defn- dispose-three-board! [this]
+  (let [{:keys [renderer resize-listener active? geometries materials textures]} (r/state this)]
+    (when active?
+      (reset! active? false))
+    (when resize-listener
+      (.removeEventListener js/window "resize" resize-listener))
+    (doseq [texture textures]
+      (.dispose texture))
+    (doseq [material materials]
+      (.dispose material))
+    (doseq [geometry geometries]
+      (.dispose geometry))
+    (when renderer
+      (let [canvas (.-domElement renderer)
+            parent (.-parentNode canvas)]
+        (when parent
+          (.removeChild parent canvas)))
+      (.dispose renderer)))
+  (r/replace-state this {}))
+
+(defn- add-table-plane! [scene geometries materials]
+  (let [geometry (js/THREE.PlaneGeometry. board-plane-size board-plane-size)
+        material (js/THREE.MeshBasicMaterial. #js {:color 0x163a31
+                                                   :side js/THREE.DoubleSide})
+        mesh (js/THREE.Mesh. geometry material)]
+    (set! (.. mesh -position -z) -0.03)
+    (.add scene mesh)
+    (swap! geometries conj geometry)
+    (swap! materials conj material)))
+
+(defn- add-card-plane! [scene loader render! active? geometries materials textures {:keys [orientation card] :as cell}]
+  (let [{:keys [image title]} card
+        geometry (js/THREE.PlaneGeometry. card-short card-long)
+        material (js/THREE.MeshBasicMaterial. #js {:color 0x202b31
+                                                   :side js/THREE.DoubleSide})
+        mesh (js/THREE.Mesh. geometry material)
+        [x y] (card-position cell)]
+    (.set (.-position mesh) x y 0.02)
+    (when (= :landscape orientation)
+      (set! (.. mesh -rotation -z) (/ js/Math.PI 2)))
+    (.add scene mesh)
+    (swap! geometries conj geometry)
+    (swap! materials conj material)
+    (let [texture (.load loader
+                         image
+                         (fn [loaded-texture]
+                           (when @active?
+                             (texture-cover! loaded-texture)
+                             (set! (.-minFilter loaded-texture) js/THREE.LinearFilter)
+                             (set! (.-magFilter loaded-texture) js/THREE.LinearFilter)
+                             (set! (.-needsUpdate loaded-texture) true)
+                             (set! (.-map material) loaded-texture)
+                             (set! (.-needsUpdate material) true)
+                             (render!)))
+                         nil
+                         (fn [error]
+                           (when @active?
+                             (js/console.error (str "Failed to load tarot texture for " title ": " image) error)
+                             (.set (.-color material) 0xc65a54)
+                             (set! (.-needsUpdate material) true)
+                             (rf/dispatch [::three-texture-error image])
+                             (render!))))]
+      (swap! textures conj texture))))
+
+(defn- mount-three-board! [this]
+  (dispose-three-board! this)
+  (rf/dispatch-sync [::clear-three-texture-errors])
+  (when (three-runtime)
+    (let [[_ cells] (r/argv this)
+          mount-node (.-boardMountNode ^js this)]
+      (when mount-node
+        (let [scene (js/THREE.Scene.)
+              camera (js/THREE.PerspectiveCamera. 45 1 0.1 100)
+              renderer (js/THREE.WebGLRenderer. #js {:antialias true})
+              loader (js/THREE.TextureLoader.)
+              active? (atom true)
+              geometries (atom [])
+              materials (atom [])
+              textures (atom [])]
+          (.setPixelRatio renderer (min 2 (or (.-devicePixelRatio js/window) 1)))
+          (.setClearColor renderer 0x0e2622 1)
+          (set! (.. renderer -domElement -className) "board-three__canvas")
+          (.appendChild mount-node (.-domElement renderer))
+          (.set (.-position camera) 0 -4.4 5.8)
+          (.lookAt camera 0 0 0)
+          (letfn [(render! []
+                    (when @active?
+                      (.render renderer scene camera)))
+                  (resize! []
+                    (when @active?
+                      (let [rect (.getBoundingClientRect mount-node)
+                            width (max 1 (.-width rect))
+                            height (max 1 (.-height rect))]
+                        (.setSize renderer width height false)
+                        (set! (.-aspect camera) (/ width height))
+                        (.updateProjectionMatrix camera)
+                        (render!))))]
+            (add-table-plane! scene geometries materials)
+            (doseq [cell cells]
+              (add-card-plane! scene loader render! active? geometries materials textures cell))
+            (.addEventListener js/window "resize" resize!)
+            (resize!)
+            (r/replace-state this {:renderer renderer
+                                   :resize-listener resize!
+                                   :active? active?
+                                   :geometries @geometries
+                                   :materials @materials
+                                   :textures @textures})))))))
+
+(def three-board-scene
+  (r/create-class
+   {:display-name "three-board-scene"
+    :component-did-mount mount-three-board!
+    :component-did-update
+    (fn [this old-argv _ _]
+      (let [old-cells (second old-argv)
+            new-cells (second (r/argv this))]
+        (when (not= old-cells new-cells)
+          (mount-three-board! this))))
+    :component-will-unmount dispose-three-board!
+    :reagent-render
+    (fn [_cells texture-errors]
+      (let [component (r/current-component)]
+        [:div.board-three
+         {:role "img"
+          :aria-label "Three-dimensional Gnostica board with nine face-up tarot territory cards"}
+         [:div.board-three__mount
+          {:aria-hidden "true"
+           :ref #(set! (.-boardMountNode ^js component) %)}]
+         (when (seq texture-errors)
+           [:p.board-3d-status.is-error
+            (str "Texture load failed for "
+                 (count texture-errors)
+                 " card"
+                 (when (not= 1 (count texture-errors)) "s")
+                 ". Check the console for image paths.")])]))}))
+
 (defn board-card [{:keys [index row col orientation card]} selected?]
   (let [{:keys [image title]} card]
     [:button.board-card
@@ -61,15 +243,21 @@
 
 (defn board-stage []
   (let [cells @(rf/subscribe [::board])
-        selected-index @(rf/subscribe [::selected-board-index])]
+        selected-index @(rf/subscribe [::selected-board-index])
+        texture-errors @(rf/subscribe [::three-texture-errors])]
     [:section.board-area
      {:data-three-revision (or (three-revision) "unavailable")}
-     [:div.board-stage
-      {:role "group"
-       :aria-label "Gnostica board"}
-      (for [cell cells]
-        ^{:key (:index cell)}
-        [board-card cell (= selected-index (:index cell))])]]))
+     (if (three-runtime)
+       [three-board-scene cells texture-errors]
+       [:div.board-fallback
+        [:p.board-3d-status.is-error
+         "Three.js is unavailable; check the pinned CDN script before /js/main.js."]
+        [:div.board-stage
+         {:role "group"
+          :aria-label "Gnostica board"}
+         (for [cell cells]
+           ^{:key (:index cell)}
+           [board-card cell (= selected-index (:index cell))])]])]))
 
 (defn territory-panel []
   (let [{:keys [row col orientation card]} @(rf/subscribe [::selected-board-cell])
